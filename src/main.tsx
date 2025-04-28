b132acc86a0a1108e15116881ef68cc979e6d74d
2UN7 <1491859758@qq.com>
2025-04-28 13:59:56 +0800

    chore: 目录结构调整

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from "react-dom/client";
 import App from "@/App";
 
-import "@/comm/styles/index.scss";
+import "@/common/styles/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   // <StrictMode>
